3e6c24397e77fad56563aa4a17c3561140d6c2bc
Shifraz Nazar <shifraznazar@gmail.com>
2023-04-10 18:41:20 +0800

    Back-end complete

--- backend/routes/workout.js
+++ backend/routes/workout.js
@@ -1,37 +1,28 @@
 const express = require("express");
 const Workout = require("../models/workoutModel");
+const {
+  createWorkout,
+  getWorkouts,
+  getWorkout,
+  deleteWorkout,
+  updateWorkout,
+} = require("../controllers/workoutController");
 
 const router = express.Router();
 
 // GET all workouts
-router.get("/", (req, res) => {
-  res.json({ message: "Hello World" });
-});
+router.get("/", getWorkouts);
 
 // GET a single workout
-router.get("/:id", (req, res) => {
-    res.json({ message: "GET a single workout" });
-});
+router.get("/:id", getWorkout);
 
 // POST a workout
-router.post("/", async (req, res) => {
-    const { title, reps, load } = req.body;
-    try{
-        const workout = await Workout.create({ title, reps, load });
-        res.status(200).json({ workout });
-    } catch (err) {
-        res.status(400).json({ message: "Error", err });
-    }   
-});
+router.post("/", createWorkout);
 
 // Delete a workout
-router.delete("/:id", (req, res) => {
-    res.json({ message: "DELETE a workout" });
-});
+router.delete("/:id", deleteWorkout);
 
 // Update a workout
-router.patch("/:id", (req, res) => {
-    res.json({ message: "UPDATE a workout" });
-});
+router.patch("/:id", updateWorkout);
 
 module.exports = router;
